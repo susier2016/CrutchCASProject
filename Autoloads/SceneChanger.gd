1d154dee574d632020f0c8445fec8896631e7cb5
Nila Jeyapriya <nilajeyapriya@gmail.com>
2021-05-01 15:11:43 -0500

    Nila - Title Screen Music

--- Autoloads/SceneChanger.gd
+++ Autoloads/SceneChanger.gd
@@ -2,11 +2,12 @@ extends CanvasLayer
 
 
 signal scene_changed()
-
 onready var animation_player = $AnimationPlayer
 onready var black = $Control/FadeToBlack
 
+
 func change_scene(path, delay = 0.5):
+	print(get_parent().get_children())
 	$Control/FadeToBlack.show()
 	yield(get_tree().create_timer(delay), "timeout")
 	animation_player.play("fade-in")
--- TitleScreen/title_screen/title_screen.gd
+++ TitleScreen/title_screen/title_screen.gd
@@ -1,9 +1,13 @@
 extends Control
+onready var titleMusic = $TitleMusic
 
 func _ready():
 	$Menu/CenterRow/Buttons/NewGameButton.grab_focus()
 	for button in $Menu/CenterRow/Buttons.get_children():
 		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
-
+		titleMusic.play()
 func _on_Button_pressed(scene_to_load):
 	SceneChanger.change_scene(scene_to_load)
+	if titleMusic.playing == true:
+		titleMusic.stop()
+	
