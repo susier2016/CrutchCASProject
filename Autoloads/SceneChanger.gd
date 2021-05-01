e271830cd6de2c0526fc5edea20dd1347f042fb5
sharanyashivani <sharanyashivani2@gmail.com>
2021-04-30 20:11:08 -0500

    sharanya - reversible fade-to-black
    created a new scene "SceneChanger" under folder Autoloads, is autoloaded in Project settings. to fade to black in any scene, call SceneChanger.change_scene(<FilePath>)

---
+++ Autoloads/SceneChanger.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+
+signal scene_changed()
+
+onready var animation_player = $AnimationPlayer
+onready var black = $Control/FadeToBlack
+
+func change_scene(path, delay = 0.5):
+	$Control/FadeToBlack.show()
+	yield(get_tree().create_timer(delay), "timeout")
+	animation_player.play("fade-in")
+	yield(animation_player, "animation_finished")
+	get_tree().change_scene(path)
+	animation_player.play("fade-out")
+	$Control/FadeToBlack.hide()
+	emit_signal("scene_changed")
