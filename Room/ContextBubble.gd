ceb0cd213d841df0e0fa831a4be3cc1719537cb9
susier2016 <susmitarenjith@gmail.com>
2021-05-02 03:16:56 -0500

    susie - fixed bugs with interaction indicator

--- Room/ContextBubble.gd
+++ Room/ContextBubble.gd
@@ -4,18 +4,23 @@ onready var animationPlayer = $HoverAnimation
 var interactable = false
 
 func _process(_delta):
-	if get_overlapping_areas().size() > 0:
-		print(true)
+	if get_overlapping_areas().size() > 0 and !get_parent().frozen:
+		interactable = true
+		self.visible = true
+		animationPlayer.play("Hover")
+	else:
+		interactable = false
+		self.visible = false
+		animationPlayer.stop(true)
 	if Input.is_action_just_pressed("ui_interact") and interactable:
 		self.visible = false
 
-func _on_ContextBubble_area_entered(_area):
-	interactable = true
-	self.visible = true
-	animationPlayer.play("Hover")
-
-func _on_ContextBubble_area_exited(_area):
-	interactable = false
-	self.visible = false
-	animationPlayer.stop(true)
-	
+#func _on_ContextBubble_area_entered(_area):
+#	interactable = true
+#	self.visible = true
+#	animationPlayer.play("Hover")
+#
+#func _on_ContextBubble_area_exited(_area):
+#	interactable = false
+#	self.visible = false
+#	animationPlayer.stop(true)
